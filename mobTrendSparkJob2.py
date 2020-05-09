e6c7ce34807b7a2331321e8e76bb012376bc2b7f
Neel Ray <lolneelsd@gmail.com>
2020-05-09 06:01:40 -0400

    Add files via upload

--- mobTrendSparkJob2.py
+++ mobTrendSparkJob2.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col
 
 spark = SparkSession\
     .builder\
@@ -16,10 +17,28 @@ df = spark.read\
 
 df.createOrReplaceTempView("mob")
 
-finaloutput = spark.sql("select * from mob where country_region_code = \"US\"")
+finalStateOutput = spark.sql("select sub_region_1, date, avg(retail_and_recreation_percent_change_from_baseline) as avg_retail_and_recreation_percent_change,\
+  avg(grocery_and_pharmacy_percent_change_from_baseline) as avg_grocery_and_pharmacy_percent_change, \
+  avg(parks_percent_change_from_baseline) as avg_parks_percent_change,\
+  avg(transit_stations_percent_change_from_baseline) as avg_transit_stations_percent_change,\
+  avg(workplaces_percent_change_from_baseline) as avg_workplaces_percent_change, \
+  avg(residential_percent_change_from_baseline) as avg_residential_percent_change\
+       from mob where country_region_code = \"US\" group by sub_region_1, date")\
+      .sort(col("date"))
 
-finaloutput.write\
+finalCityOutput = spark.sql("select sub_region_2, date, retail_and_recreation_percent_change_from_baseline\
+    grocery_and_pharmacy_percent_change_from_baseline,  parks_percent_change_from_baseline\
+    transit_stations_percent_change_from_baseline,  workplaces_percent_change_from_baseline\
+    residential_percent_change_from_baseline from mob where country_region_code = \"US\"").sort(col("date"))
+
+finalStateOutput.write\
     .format("com.mongodb.spark.sql.DefaultSource") \
     .mode("append") \
-    .option("collection", "job_collections2") \
+    .option("collection", "state") \
+    .save()
+
+finalCityOutput.write\
+    .format("com.mongodb.spark.sql.DefaultSource") \
+    .mode("append") \
+    .option("collection", "city") \
     .save()
