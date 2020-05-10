b365d8b864966c99db969d142809a48dfef88136
Akansha Priya <akanshaiam94@gmail.com>
2020-05-09 21:31:29 -0400

    format changes

--- twitter-data.py
+++ twitter-data.py
@@ -2,6 +2,7 @@ from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
 from tweepy import Stream
 from kafka.producer import KafkaProducer
+import json
 
 access_token = "1374576763-MJG1pBwCZllwGIoYQWw2XkNIEOtXcUgjp3mvA5t"
 access_token_secret =  "zopeHyvzMASoZe6TcOBQjkKJSeozDtqGtwngeNX23hoUz"
@@ -14,11 +15,50 @@ filter_keywords = ["Coronavirusmexico","covid2019","coronavirususa","covid_19uk"
 
 class StdOutListener(StreamListener):
     def on_data(self, data):
-        producer.send("COVID19", data.encode('utf-8'))
+        json_data = json.loads(data)
+        new_data = format_twitter_data(json_data)
+        producer.send("COVID19", (str(new_data)).encode('utf-8'))
         #print (data)
         return True
     def on_error(self, status):
         print (status)
+
+def format_twitter_data(json_data):
+    new_data = {
+        "created_at" : json_data["created_at"],
+        "id" : json_data["id"],
+        "text" : json_data["text"],
+        "coordinates" : json_data["coordinates"],
+        "place" : json_data["place"],
+        "reply_count" : json_data["reply_count"],
+        "retweet_count" : json_data["retweet_count"],
+        "entities" : json_data["entities"],
+        "favorited" : json_data["favorited"],
+        "retweeted" : json_data["retweeted"],
+        "lang" : json_data["lang"]
+    }
+    if "user" in json_data:
+        new_data["user"] = {
+            "location" : json_data["user"]["location"],
+            "geo-enabled" : json_data["user"]["geo_enabled"],
+            "verified" : json_data["user"]["verified"],
+            "description" : json_data["user"]["description"]
+        }
+    
+    if "retweeted_status" in json_data:
+        new_data["retweeted_status"] = {
+            "text" : json_data["retweeted_status"]["text"]
+        }
+        if "user" in json_data["retweeted_status"]:
+            new_data["retweeted_status"]["user"] = {
+                "id" : json_data["retweeted_status"]["user"]["id"],
+                "location" : json_data["retweeted_status"]["user"]["location"]
+            }
+        if "extended_tweet" in json_data["retweeted_status"]:
+             new_data["retweeted_status"]["extended_tweet"] = {
+                 "full_text" : json_data["retweeted_status"]["extended_tweet"]["full_text"]
+             }
+    return new_data
 
 producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
 l = StdOutListener()
